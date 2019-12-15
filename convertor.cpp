34937b70e8a814bfaf52dd930015685c50b31c50
Prasun <prasun.code@gmail.com>
2019-12-15 09:39:13 +0530

    Added BtoD

--- convertor.cpp
+++ convertor.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 bool checkBinary(std::string str){
     for(char c: str){
@@ -30,9 +31,13 @@ bool checkHex(std::string str){
     return true;
 }
 
-//std::string binarytoDecimal(std::string str){
-
-//}
+std::string binarytoDecimal(std::string str){
+    long long int dec = 0;
+    for(int i = 0; i < str.size(); ++i){
+        dec += (str[i] - '0') * (long long int)pow(2,str.size()-1-i);
+    }
+    return std::to_string(dec);
+}
 
 int main(){
     std::cout << checkBinary("111000111") << std::endl;
@@ -41,5 +46,8 @@ int main(){
     std::cout << checkHex("11100BCD25MFA111") << std::endl;
     std::cout << checkDecimal("-111006464694111") << std::endl;
 
-
+    std::cout << binarytoDecimal("11001") << std::endl;
+    std::cout << binarytoDecimal("0") << std::endl;
+    std::cout << binarytoDecimal("1") << std::endl;
+    std::cout << binarytoDecimal("0000111111111111111111111111100000000000000000000000000000010") << std::endl;
 }
